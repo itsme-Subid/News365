4a74ddb4c0e9406cacd4f42a9843e44a7c3e4e4e
SUBID DAS <itsmesubid@gmail.com>
2022-10-07 15:42:54 +0530

    Organize `response.json` from api

--- src/components/News/NewsItem.jsx
+++ src/components/News/NewsItem.jsx
@@ -1,27 +1,53 @@
+import "./NewsItem.css";
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
     let { id, news } = this.props;
+    let pubDate = new Date(news.pubDate);
+    let nowDate = new Date();
     return (
       <>
-        <div
-          className="card my-5"
-          key={id}
-          style={{ color: "rgb(var(--dark-color))" }}
-        >
+        <div className="card" key={id}>
           {news.image_url && (
-            <img src={news.image_url} className="card-img-top" alt="..." />
+            <div
+              className="card-img-top"
+              style={{ width: "100%", height: "200px" }}
+            >
+              <img src={news.image_url} alt="..." />
+            </div>
           )}
           <div className="card-body">
+            {news.source_id && (
+              <span
+                className="card-source"
+                title={`News source: ${news.source_id.toUpperCase()}`}
+              >
+                {news.source_id}
+              </span>
+            )}
             <h5 className="card-title">
-              {news.title}{" "}
-              <span className="badge bg-primary">{news.source_id}</span>
+              <span>{news.title} </span>
             </h5>
-            <p className="card-text">{news.content}</p>
+            <p className="card-text">{news.description}</p>
             <p className="card-text">
-              <small className="text-muted">Date: {news.pubDate}</small>
+              <small className="text-muted">
+                {((nowDate - pubDate) / 1000 / 60 / 60).toPrecision(2) +
+                  " Hours ago"}
+              </small>
             </p>
+            <div className="card-footer">
+              {news.link && (
+                <a
+                  href={news.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="card-button"
+                >
+                  Read more
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </>
