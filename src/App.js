c239ecbbfc417cc15a13bfc8580ead585f12c8e6
SUBID DAS <itsmesubid@gmail.com>
2022-10-08 02:25:40 +0530

    Minor `optimization`

--- src/App.js
+++ src/App.js
@@ -14,12 +14,24 @@ export class App extends Component {
     };
   }
   render() {
+    console.clear();
+    console.log(
+      "%cStop!",
+      "font-family: sans-serif; font-weight: 700; color: red; font-size: 5rem; text-align: center; text-transform: uppercase; width: 100%;"
+    );
+    console.log(
+      `%cThis is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Calculator.js feature or "hack" this server, it is a scam and will give them access to your device.`,
+      `font-size: 1.2rem; font-family: sans-serif; color: #fff`
+    );
+    console.log(
+      "%citsme-subid",
+      "font-family: sans-serif; font-weight: 700; color: #1f6feb; padding-block: 2rem; font-size: 2.5rem; text-align: center; text-transform: uppercase; width: 100%;"
+    );
     let menuToggle = () => {
       this.setState({ menuOpen: !this.state.menuOpen });
     };
     let changeCategory = (category) => {
       this.setState({ category: category });
-      console.log(category);
     };
     return (
       <>
